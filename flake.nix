03242580f9b6ae772ff0b6ae2449203c2db94410
Hossein Naderi <hossein-naderi@hotmail.com>
2024-06-07 16:25:53 +0330

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Sample micropython build";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+        mpy = pkgs.callPackage ./firmware.nix { };
+      in { devShells.default = pkgs.mkShell { packages = [ mpy ]; }; });
+}
